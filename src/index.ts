36024cf1ceffd949d238bac28fe17dfef2770064
JunoLee <aaaa5232@nate.com>
2026-09-21 14:47:28 +0900

    add PORT 8080 for option

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,8 @@ import { env } from './config/env'
 import { logger } from './lib/logger'
 import { startScheduler } from './lib/scheduler'
 
+const PORT = process.env.PORT || 8080
 app.listen(env.port, '0.0.0.0', () => {
-  logger.info(`Server listening on port ${env.port} [${env.nodeEnv}]`)
+  logger.info(`Server listening on port ${PORT} [${env.nodeEnv}]`)
   startScheduler()
 })
