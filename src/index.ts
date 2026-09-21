8c0597ab14a9c7926d5ba194d8a54f5a58d9b60e
JunoLee <aaaa5232@nate.com>
2026-09-21 19:49:09 +0900

    fix/index added PORT type

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import { env } from './config/env'
 import { logger } from './lib/logger'
 import { startScheduler } from './lib/scheduler'
 
-const PORT = process.env.PORT || 8080
-app.listen(env.port, '0.0.0.0', () => {
+const PORT: number = Number(process.env.PORT) || 8080
+app.listen(PORT, '0.0.0.0', () => {
   logger.info(`Server listening on port ${PORT} [${env.nodeEnv}]`)
   startScheduler()
 })
